ca985da1287b77dd9d9f60e6f6a2396b5d7772f4
agomez1996 <117057785+agomez1996@users.noreply.github.com>
2022-11-16 04:21:03 +0000

    Add files via upload

---
+++ comparar.js
@@ -0,0 +1,44 @@
+function pi(){
+    a = Math.PI
+    var m = parseInt(document.getElementById("m").value);
+    ec=1
+    for(i=1;i<=m;i++){
+        ec=ec*(Math.pow((2*i),2))/((Math.pow(2*i,2))-1);
+
+    }
+    ec1=2*ec
+    document.getElementById('respi').innerHTML="La ecuacion evaluada con m = "+m+ " es " +ec1+" es igual a "+a+"?"
+}
+
+function BMI(){
+    var libra = parseInt(document.getElementById("libra").value);
+    
+    var pie = parseInt(document.getElementById("pie").value);
+    var pie1 = pie*12;
+    var pulgada = parseInt(document.getElementById("pulgada").value);
+    var sum = pie1+pulgada;
+    var form = 703*(libra/Math.pow(sum,2));
+    
+    if (form < 18.5){
+        document.getElementById('masa').innerHTML="su valor de BMI es: " +form+ " , lo que lo clasifica como bajo peso, donde " +form+ " es el valor de BMI redondeado a la decima mas cercana y bajo peso es la clasificacion correspondiente"
+
+    }
+    else if(form >=18.5 && form <= 24.9){
+
+        document.getElementById('masa').innerHTML="su valor de BMI es: " +form+ ", lo que lo clasifica como peso normal, donde " +form+ " es el valor de BMI redondeado a la decima mas cercana y peso normal es la clasificacion correspondiente"
+        
+    }
+    else if (form >=25 && form <= 29.9){
+
+        document.getElementById('masa').innerHTML="su valor de BMI es: " +form+ ", lo que lo clasifica como exeso de peso, donde " +form+ " es el valor de BMI redondeado a la decima mas cercana y exeso de peso es la clasificacion correspondiente"
+        
+ 
+
+    }
+    else if(form < 30){
+
+        document.getElementById('masa').innerHTML="su valor de BMI es: " +form+ " , lo que lo clasifica como obecidad, donde " +form+ " es el valor de BMI redondeado a la decima mas cercana y obesidad es la clasificacion correspondiente"
+        
+    }
+
+}
